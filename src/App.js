548179c65bee57b6037c2bbf960ea646255eba7e
UshaPeketi <91246576+UshaPeketi@users.noreply.github.com>
2023-03-29 14:40:53 +0530

    Calculator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useEffect, useState} from 'react'
+import './App.css'
+const App = () => {
+  const [input,setInput]=useState("")
+  const [mainData,setMaindata]=useState("")
+  const handler=e=>{
+    setInput(e.target.value)
+  }
+//   function evil(fn) {
+//     return new Function('return ' + fn)();
+//   }
+// console.log( evil('12/2+5') ); // => 11
 
-function App() {
+//alternative for each onClick() in button -->function()---><button onClick={handClick} />
+// const handClick = e=>{
+//   setInput(input.concat(e.target.name));
+// }
+useEffect(()=>{
+  localStorage.setItem("data",JSON.stringify(input))
+},[input])
+useEffect(()=>{
+  const mainData=JSON.parse(localStorage.getItem("data"))
+  setMaindata("usha")
+},[])
+
+const backSpace=()=>{
+  setInput(input.slice(0,-1));
+}
+const calculate=()=>{
+  try{
+  setInput(eval(input).toString());
+}
+catch(err){
+  setInput("Error")
+}
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='data'>
+      {mainData}
+      </div>
+      
+      <center><br/><br/><br/><br/><br/><br/><br/><br/>
+      <div  className="container"><br/>
+        <input type="text" name="input" value={input} onChange={handler}/><br/><br/>
+        {/* <button name="input" onClick={calculate}>Result</button><br/>
+        <h3>Result is :{input}</h3> */}
+        <div className='keypad'>
+        <button onClick={()=>setInput('')} id="clear">clear</button>
+        <button onClick={backSpace}>c</button>
+        <button onClick={()=>setInput(input+'/')}>&divide;</button>
+
+        <button onClick={()=>setInput(input+'7')}>7</button>
+        <button onClick={()=>setInput(input+'8')}>8</button>
+        <button onClick={()=>setInput(input+'9')}>9</button>
+        <button onClick={()=>setInput(input+'*')}>&times;</button>
+
+        <button onClick={()=>setInput(input+'4')}>4</button>
+        <button onClick={()=>setInput(input+'5')}>5</button>
+        <button onClick={()=>setInput(input+'6')}>6</button>
+        <button onClick={()=>setInput(input+'-')}>&ndash;</button>
+
+        <button onClick={()=>setInput(input+'1')}>1</button>
+        <button onClick={()=>setInput(input+'2')}>2</button>
+        <button onClick={()=>setInput(input+'3')}>3</button>
+        <button onClick={()=>setInput(input+'+')}>+</button>
+
+        <button onClick={()=>setInput(input+'0')}>0</button>
+        <button onClick={()=>setInput(input+'.')}>.</button>
+        <button onClick={calculate} id="result">=</button>
+        </div>
+        </div>
+      </center>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
